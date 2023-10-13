104e185bcfc0d2d212f71f983ee4371c461a0f83
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-10-13 19:28:13 +0200

    ♻️ Replace Tab navigator by Stack and use a tab component

--- front/app/_layout.tsx
+++ front/app/_layout.tsx
@@ -1,9 +1,10 @@
 import { OpenSans_400Regular } from "@expo-google-fonts/open-sans";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { loadAsync } from "expo-font";
-import { SplashScreen, Tabs } from "expo-router";
-import { Home } from "lucide-react-native";
+import { SplashScreen, Stack } from "expo-router";
 import { useCallback, useEffect, useState } from "react";
+
+import BottomTabNavigation from "../components/BottomTabNavigation";
 
 export default function RootLayout() {
   const [appIsReady, setAppIsReady] = useState(false);
@@ -32,24 +33,8 @@ export default function RootLayout() {
 
   return (
     <GluestackUIProvider>
-      <Tabs screenOptions={{ headerShown: false }}>
-        <Tabs.Screen
-          name="index"
-          options={{
-            href: "/",
-            title: "Home",
-            tabBarIcon: () => <Home color="#84CC16" size={25} />,
-            tabBarLabelStyle: {
-              color: "#525252",
-              marginTop: 0,
-              fontSize: 12,
-              marginBottom: 6,
-            },
-          }}
-        />
-        <Tabs.Screen name="other" options={{ href: null }} />
-        <Tabs.Screen name="offers/[id]" options={{ href: null }} />
-      </Tabs>
+      <Stack initialRouteName="/home" screenOptions={{ headerShown: false }} />
+      <BottomTabNavigation />
     </GluestackUIProvider>
   );
 }
