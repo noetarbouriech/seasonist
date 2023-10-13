3a1379df83c7f01843429d474c86a08dfd33aac8
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-10-13 19:26:59 +0200

    🚚 Move homepage in another file
    (redirect logic in index page)

--- front/app/index.tsx
+++ front/app/index.tsx
@@ -1,13 +1,7 @@
-import { ScrollView } from "@gluestack-ui/themed";
+import { router } from "expo-router";
 
-import Header from "../components/Header";
-import OfferList from "../components/offers/OfferList";
+export default function Index() {
+  router.replace("/home");
 
-export default function Home() {
-  return (
-    <ScrollView w="$full" minHeight="$full" bgColor="$white">
-      <Header title="👋 Welcome" search={{ placeholder: "Search an offer" }} />
-      <OfferList title="Recommended for you" />
-    </ScrollView>
-  );
+  return null;
 }
